20e45d0d333bbbf9a62226faf7bc44e45b510991
jiraz <jiraz@LAPTOP-T07DQ2M5>
2019-07-10 10:58:47 -0500

    Nuevo inicio

---
+++ src/pkgfinal/Final.java
@@ -0,0 +1,14 @@
+
+package pkgfinal;
+
+import java.util.Scanner;
+public class Final {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner (System.in);
+     //new Inicio("aux");
+     Frame z=new Frame();
+     z.setVisible(true);
+    }
+    
+}
